535c58e4639efabc0a0e300ff5853cc6ee0066fc
Leidy Castaño <leidy.castanoc@udea.edu.co>
2022-08-21 11:54:39 -0500

    Modulo de Producto

---
+++ src/main/java/com/ventas/ventas/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.ventas.ventas.repository;
+
+import com.ventas.ventas.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    Optional<Producto> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
+}
